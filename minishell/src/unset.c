38df9fbe450bbd26d94d153382c745f33ae9f768
MarJC5 <jmartin@student.42lausanne.ch>
2022-03-02 12:56:35 +0100

    Builtin - Begin export

--- minishell/src/unset.c
+++ minishell/src/unset.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/01 12:53:58 by jmartin           #+#    #+#             */
-/*   Updated: 2022/03/02 12:48:21 by jmartin          ###   ########.fr       */
+/*   Created: 2022/03/02 09:27:36 by jmartin           #+#    #+#             */
+/*   Updated: 2022/03/02 09:33:16 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
-
-void	get_export(int args_count, char *cmd, char *args_value, char **list);
-
-int	main(int argc, char **argv, char **envp)
-{
-	if (argc >= 2)
-		get_export(argc, argv[1], argv[2], envp);
-	return (0);
-}

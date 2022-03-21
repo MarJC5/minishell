3a3db76818eebd463bbc33ca0ca19a6a77618dca
Jean-Christio Martin <admin@marjc.ch>
2022-03-21 09:19:13 +0100

    Fix

--- minishell/src/builtins/env.c
+++ minishell/src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 16:42:05 by jmartin           #+#    #+#             */
-/*   Updated: 2022/03/16 14:07:26 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/03/21 09:17:45 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,18 @@ void	env(t_shell *shell)
 	int	i;
 
 	i = -1;
-	while (shell->envp[++i])
+	if (shell->cmd->args_count > 1)
 	{
-		if (shell->envp[i + 1])
-			ft_printf("%s\n", shell->envp[i]);
-		else
-			ft_printf("_=/usr/bin/env\n");
+		str_err("env: ", NULL);
+		perror(shell->cmd->args[1]);
+	}
+	else
+	{
+		while (shell->envp[++i])
+		{
+			ft_printf("%s", shell->envp[i]);
+			if (shell->envp[i + 1] != NULL)
+				ft_printf("\n");
+		}
 	}
 }

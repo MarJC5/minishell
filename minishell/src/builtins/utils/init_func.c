e768298d7db63c155cf67daa2a58d8c18c2e7cc5
Jean-Christio Martin <admin@marjc.ch>
2022-06-10 07:19:39 +0200

    Fix parsing cmd name with quote

--- minishell/src/builtins/utils/init_func.c
+++ minishell/src/builtins/utils/init_func.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_func.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartin <jmartin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 00:43:58 by jmartin           #+#    #+#             */
-/*   Updated: 2022/06/09 10:33:50 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/06/10 07:03:20 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,10 @@ void	init_func(t_shell *shell, int i)
 void	builtin_error(t_shell *shell, int cmd_index)
 {
 	g_exit_stat = 127;
-	str_err("minishell: command not found: ",
-		shell->cmd[cmd_index]->name);
+	if (quote_counter(shell->cmd[cmd_index]->name, '\'') == 1
+		|| quote_counter(shell->cmd[cmd_index]->name, '\"') == 1)
+		str_err("minishell: syntax error", NULL);
+	else
+		str_err("minishell: command not found: ",
+			shell->cmd[cmd_index]->name);
 }
--- minishell/src/parsing/quote_utils.c
+++ minishell/src/parsing/quote_utils.c
@@ -3,14 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   quote_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartin <jmartin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 16:14:50 by jmartin           #+#    #+#             */
-/*   Updated: 2022/06/09 19:45:19 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/06/10 07:16:50 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+char	*remove_char(char *str, int start, int end);
+
+int	ft_strchr_pos(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < ft_strlen(str))
+	{
+		if (str[i] == (char)c)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strrchr_pos(const char *str, int c)
+{
+	size_t	i;
+
+	i = ft_strlen(str);
+	while (i)
+	{
+		if (str[i] == (char)c)
+			return (i);
+		i--;
+	}
+	return (0);
+}
 
 int	quote_counter(char *s, char c)
 {
@@ -29,69 +59,77 @@ int	quote_counter(char *s, char c)
 	return (EXIT_SUCCESS);
 }
 
-char	*remove_char(char *str, char garbage, char keep)
+char	*remove_quote_out(char *str, char c)
+{
+	int	start;
+	int	end;
+
+	start = ft_strchr_pos(str, c);
+	end = ft_strrchr_pos(str, c);
+	if (str[start] == c && str[end] == c)
+		str = remove_char(str, start, end);
+	return (str);
+}
+
+char	*remove_quote_in(char *str, char c)
+{
+	int	start;
+	int	end;
+
+	start = ft_strchr_pos(str, c);
+	end = ft_strrchr_pos(str, c);
+	if (str[start] == c && str[end] == c && quote_counter(str, c))
+		str = remove_char(str, start, end);
+	return (str);
+}
+
+char	*remove_char(char *str, int start, int end)
 {
 	char	*src;
 	char	*dst;
+	int		i;
 
+	i = 0;
 	dst = str;
 	src = dst;
 	while (*src != '\0')
 	{
 		*dst = *src;
-		if (*dst != garbage && *dst + 1 != keep)
+		if (i != start && i != end)
 			dst++;
 		src++;
+		i++;
 	}
 	*dst = '\0';
 	return (dst);
-}
-
-char	*remove_dquote_out(char *str)
-{
-	int		start;
-	size_t	end;
-
-	start = 0;
-	end = ft_strlen(str) - 1;
-	if (quote_counter(str, '\"') != 1)
-	{
-		if (ft_strchr(str, '\"') 
-			&& ft_strchr(str, '\'') == NULL)
-			if (str[start] == '\"' && str[end] == '\"')
-				str = remove_char(str, '\"', '\'');
-	}
-	return (str);
-}
-
-char	*remove_squote_out(char *str)
-{
-	int		start;
-	size_t	end;
-
-	start = 0;
-	end = ft_strlen(str) - 1;
-	if (quote_counter(str, '\'') != 1)
-	{
-		if (ft_strchr(str, '\'') 
-			&& ft_strchr(str, '\"') == NULL)
-			if (str[start] == '\'' && str[end] == '\'')
-				str = remove_char(str, '\'', '\"');	
-	}
-	return (str);
 }
 
 
 void	cmd_parsing(t_shell *shell, int cmd_index)
 {
 	int		i;
+	size_t	end;
 
 	i = 0;
-	while (shell->cmd[cmd_index]->args[i])
+	end = ft_strlen(shell->cmd[cmd_index]->args[i]) - 1;
+	if (shell->cmd[cmd_index]->args[i][0] == '\"'
+		&& shell->cmd[cmd_index]->args[i][end] == '\"')
 	{
-
-		remove_dquote_out(shell->cmd[cmd_index]->args[i]);
-		remove_squote_out(shell->cmd[cmd_index]->args[i]);
+		if (quote_counter(shell->cmd[cmd_index]->args[i], '\"') == 0)
+			while (ft_strchr(shell->cmd[cmd_index]->args[i], '\"'))
+				remove_quote_out(shell->cmd[cmd_index]->args[i], '\"');
+	}
+	else if (shell->cmd[cmd_index]->args[i][0] == '\''
+		&& shell->cmd[cmd_index]->args[i][end] == '\'')
+	{
+		if (quote_counter(shell->cmd[cmd_index]->args[i], '\'') == 0)
+			while (ft_strchr(shell->cmd[cmd_index]->args[i], '\''))
+				remove_quote_out(shell->cmd[cmd_index]->args[i], '\'');
+	}
+	while (shell->cmd[cmd_index]->args[++i])
+	{
+		//remove_quote_in(shell->cmd[cmd_index]->args[i], '\"');
+		//remove_quote_in(shell->cmd[cmd_index]->args[i], '\'');
 		i++;
 	}
 }

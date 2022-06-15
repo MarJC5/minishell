c12e77154c34487222315917c859177a148464f8
Jean-Christio Martin <admin@marjc.ch>
2022-06-15 08:38:16 +0200

    Update echo

--- minishell/src/builtins/utils/echo_utils.c
+++ minishell/src/builtins/utils/echo_utils.c
@@ -3,23 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   echo_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmartin <jmartin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 15:54:09 by jmartin           #+#    #+#             */
-/*   Updated: 2022/06/14 17:44:13 by jmartin          ###   ########.fr       */
+/*   Updated: 2022/06/15 08:37:50 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
 
+static void	pars_echo_trim(t_shell *shell, char *args, int cmd_index, int i)
+{
+	size_t	trim;
+	size_t	start;
+	char	*tmp;
+	char	*tmp2;
+
+	start = ft_strlen(shell->cmd[cmd_index]->name) + 1;
+	trim = ft_strlen(shell->cmd[cmd_index]->args[i]);
+	ft_printf("--> arg_to_trim = %s, arg_to_join = %s\n", shell->cmd[cmd_index]->args[i], args);
+	if (shell->bcklash_n == 1)
+		start += 3;
+	if (trim > 0)
+	{
+		tmp = ft_substr(shell->cmd[cmd_index]->args[i], start, trim);
+		ft_printf("start = %i, trim = %i --> %s\n", start, trim, tmp);
+		free(tmp);
+		tmp2 = ft_strdup(args);
+		free(args);
+		args = ft_strjoin(ft_strtrim(tmp, "\""), ft_strtrim(tmp2, "\""));
+		free(tmp2);
+		append('\"', args, '\"');
+	}
+}
+
 void	pars_echo_endl(t_shell *shell, char **args, int cmd_index, int start)
 {
-	size_t	j;
 	size_t	i;
 	int		find;
-	
+
 	i = 0;
-	j = 0;
 	find = 0;
 	while (args[start][i])
 	{
@@ -28,16 +51,18 @@ void	pars_echo_endl(t_shell *shell, char **args, int cmd_index, int start)
 			find++;
 		i++;
 	}
-    if (start == 0 && args[start][i] == '\0')
-    {
-        ft_printf("--> %d\n", i - (ft_strlen(shell->cmd[cmd_index]->name) + find + 1));
-        shell->cmd[cmd_index]->start = 1;
-        shell->bcklash_n = 1;
-    }
+	if (start == 0 && args[start][i] == '\0')
+	{
+		shell->cmd[cmd_index]->start = 1;
+		shell->bcklash_n = 1;
+	}
 	else if (args[start][find + 1] == '\0')
 	{
-        ft_printf("--> %d\n", i - ft_strlen(shell->cmd[cmd_index]->name));
 		shell->cmd[cmd_index]->start = 2;
 		shell->bcklash_n = 1;
 	}
+	if (shell->bcklash_n == 1)
+		pars_echo_trim(shell, args[start - 1], cmd_index, start);
+	else
+		pars_echo_trim(shell, args[start], cmd_index, start - 1);
 }
